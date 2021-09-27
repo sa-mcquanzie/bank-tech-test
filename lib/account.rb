7a670935897ab8368845e21a03ca8b69c7cf0181
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-09-27 13:16:24 +0100

    Add initial balance to account

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,7 @@
 class Account
   attr_reader :balance
-  
-  def initialize
-    @balance = 0
+
+  def initialize(balance = 0)
+    @balance = balance
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,11 +1,19 @@
 require 'account'
 
-describe 'when a user opens a bank account' do
-  context 'without providing and money' do
+describe 'A bank account' do
+  context 'opened with no initial balance' do
     let(:new_empty_account) { Account.new }
 
     it 'should have a balance of 0' do
       expect(new_empty_account.balance).to be(0)
+    end
+  end
+
+  context 'opened with an initial balance' do
+    let(:new_account_with_money) { Account.new(100) }
+
+    it 'should contain the initial balance' do
+      expect(new_account_with_money.balance).to be(100)
     end
   end
 end
