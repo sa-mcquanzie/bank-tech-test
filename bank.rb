61189ca859bf878f524ab20397008c61c4adc55c
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-09-28 16:21:57 +0100

    Remove duplicate bank.rb from lib

--- bank.rb
+++ bank.rb
@@ -3,9 +3,3 @@ require_relative 'lib/statement'
 require_relative 'lib/transaction'
 
 @account = Account.new
-
-def deposit(amount) @account.deposit(amount) end
-
-def withdraw(amount) @account.withdraw(amount) end
-
-def statement() @account.statement.show end
--- lib/bank.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'account'
-require_relative 'statement'
-require_relative 'transaction'
-
-@account = Account.new
